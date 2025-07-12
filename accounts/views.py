8badf102876496cfb5a622e3961aa09672a47326
AliJalili <pandaj200021@gmail.com>
2025-07-12 23:37:51 +0330

    UserLogoutView added

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,8 @@ from rest_framework.views import APIView
 from .serializers import RegisterSerializer
 from .models import User
 from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import IsAuthenticated
 
 
 class UserRegisterView(APIView):
@@ -15,5 +17,13 @@ class UserRegisterView(APIView):
         return Response(data=ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class UserLogoutView(APIView):
+    permission_classes = [IsAuthenticated,]
 
-
+    def post(self, request):
+        try:
+            token = RefreshToken(token=request.data['refresh'])
+            token.blacklist()
+            return Response({'detail':'Logged out successful'}, status=status.HTTP_205_RESET_CONTENT)
+        except Exception as err:
+            return Response({'error':str(err)}, status=status.HTTP_400_BAD_REQUEST)
