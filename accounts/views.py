e7aa6855c6a854cf6b34428390191b2a81267e74
AliJalili <pandaj200021@gmail.com>
2025-07-12 18:25:48 +0330

    UserRegisterView added

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import RegisterSerializer
+from .models import User
+from rest_framework import status
 
-# Create your views here.
+
+class UserRegisterView(APIView):
+
+    def post(self, request):
+        ser_data = RegisterSerializer(data=request.data)
+        if ser_data.is_valid():
+            ser_data.save()
+            return Response(data=ser_data.data, status=status.HTTP_201_CREATED)
+        return Response(data=ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
