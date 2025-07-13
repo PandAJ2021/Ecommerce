a6fa7aae605b460206330f5418311dcc8df562ff
AliJalili <pandaj200021@gmail.com>
2025-07-13 23:55:36 +0330

    add product views: list, detail, and admin viewset

--- products/views.py
+++ products/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.viewsets import ModelViewSet
+from .models import Product
+from .serializers import ProductAdminSerializer, ProductDetailSerializer, ProductPublicSerializer
+from rest_framework.permissions import IsAdminUser
 
-# Create your views here.
+class ProductListView(ListAPIView):
+    queryset = Product.objects.filter(is_available=True)
+    serializer_class = ProductPublicSerializer
+
+class ProductDetailView(RetrieveAPIView):
+    queryset = Product.objects.filter(is_available=True)
+    serializer_class = ProductDetailSerializer
+    lookup_field = 'slug'
+
+class ProductAdminViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductAdminSerializer
+    permission_classes = [IsAdminUser]
