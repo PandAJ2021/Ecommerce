2c66fa7bd29ba75d0311853c2c83c41534a4fb5a
AliJalili <pandaj200021@gmail.com>
2025-07-14 22:32:14 +0330

    register cart and cart-item viewsets using SimpleRouter

--- cart/urls.py
+++ cart/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from rest_framework import routers
+from . import views
+
+router = routers.SimpleRouter()
+router.register(r'', views.CartViewSet, basename='cart')
+router.register(r'cart-items', views.CartItemViewSet, basename='cartitem')
 
 app_name = 'cart'
-urlpatterns = [
-    
-]
+urlpatterns = [] 
+urlpatterns+= router.urls
