ab94d7f22d6f105f087c6157cb7e5ce2e609ece1
AliJalili <pandaj200021@gmail.com>
2025-07-14 22:28:41 +0330

    CartViewSet and CartItemViewSet added

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import CartSerializer, CartItemSerializer
+from rest_framework.permissions import IsAuthenticated
+from .models import Cart, CartItem
+from rest_framework.exceptions import ValidationError
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+class CartViewSet(viewsets.ModelViewSet):
+    serializer_class = CartSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Cart.objects.all()
+        return Cart.objects.filter(user=user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)    
+
+
+class CartItemViewSet(viewsets.ModelViewSet):
+    serializer_class = CartItemSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return CartItem.objects.filter(cart__user=self.request.user, cart__is_active=True)
+    
+    def perform_create(self, serializer):
+        try:
+            cart = get_object_or_404(Cart, user=self.request.user, is_active=True)
+        except:
+            raise ValidationError('No active cart found for user')
+        
+        serializer.save(cart=cart)
+
