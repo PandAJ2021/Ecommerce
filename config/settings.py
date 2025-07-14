122eaa1575a57883e5329a47ee2636c593a88529
AliJalili <pandaj200021@gmail.com>
2025-07-14 22:36:09 +0330

    register cart app

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     'rest_framework_simplejwt.token_blacklist',
     'products.apps.ProductsConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
