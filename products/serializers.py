f6cbdbd0ae254054f8da54f4768df4dc4a646185
AliJalili <pandaj200021@gmail.com>
2025-07-14 14:46:30 +0330

    readability of category improved

--- products/serializers.py
+++ products/serializers.py
@@ -1,17 +1,18 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import Product
 
-class ProductAdminSerializer(ModelSerializer):
+class ProductAdminSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
 
-class ProductPublicSerializer(ModelSerializer):
+class ProductPublicSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['name', 'price', 'is_available', 'image']
 
-class ProductDetailSerializer(ModelSerializer):
+class ProductDetailSerializer(serializers.ModelSerializer):
+    category = serializers.StringRelatedField()
     class Meta:
         model = Product
         fields = ['name', 'slug', 'category', 'description', 'price', 'image']
