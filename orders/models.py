8744341aa0ceaf29738087ff89cc4e7535b5dd02
AliJalili <pandaj200021@gmail.com>
2025-07-15 20:14:54 +0330

    Address and Order models added

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from accounts.models import User
+from cart.models import Cart
 
-# Create your models here.
+
+class Address(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='addresses')
+    province = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    street = models.CharField(max_length=255)
+    code = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return f'{self.province} | {self.city} | street:{self.street} | code:{self.code}'
+    
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='orders')
+    address = models.ForeignKey(Address, on_delete=models.SET_NULL, null=True, related_name='orders')
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=[
+        ('pending','pending'),
+        ('paid','paid'),
+        ('processing','processing'),
+        ('shipped','shipped'),
+        ('delivered', 'delivered'),
+        ('cancelled', 'cancelled')],
+         default= 'pending')
+    
+    def __str__(self):
+        return f"Order #{self.id} by {self.user} - {self.status}"
