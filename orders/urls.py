0b10ea0c6ef76a433383c0bfe8895c9e6d641252
AliJalili <pandaj200021@gmail.com>
2025-07-16 17:53:49 +0330

    register order and address viewsets using SimpleRouter

--- orders/urls.py
+++ orders/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
+from rest_framework import routers
+from . import views
+
+router = routers.SimpleRouter()
+router.register(r'address', views.AddressViewSet, basename='address')
+router.register(r'', views.OrderViewSet, basename='order')
 
 app_name = 'orders'
 urlpatterns = []
+urlpatterns += router.urls
